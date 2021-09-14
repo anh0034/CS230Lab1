36df2c5c61b9eea66a899a0739f6ad8df28d4031
anh0034 <hayandrew2@gmail.com>
2021-09-14 18:15:43 -0400

    Added Two Layouts, Main(top bars) and Body-Top(Other 2 Components). Split Navigation bars into seperate componenets under headers folder. Removed Uneccesary css and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import {TopBarComponent} from './headers/top-bar.component';
-import {FirstBarComponent} from './first-bar.component';
-import { SecondBarComponent } from './second-bar.component';
+import { SecondBarComponent } from '../Body/second-bar.component';
+import { FirstBarComponent } from '../Body/first-bar.component';
+import { TopBarComponent } from '../headers/top-bar.component';
+import { TopBarComponent2 } from '../headers/top-bar2.component';
+import { LayoutMain } from '../Layout/layout-main.component';
+import { LayoutTopBody } from '../Layout/layout-top-body.component';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TopBarComponent, FirstBarComponent, SecondBarComponent],
+  declarations: [ AppComponent, TopBarComponent, FirstBarComponent, SecondBarComponent, TopBarComponent2, LayoutMain, LayoutTopBody],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
